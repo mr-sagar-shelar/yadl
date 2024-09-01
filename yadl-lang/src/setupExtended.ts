56d2a4745bd5e78711ea2586890744434a491d6d
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2024-09-01 09:51:03 +0530

    Added yadl language configs

---
+++ yadl-lang/src/setupExtended.ts
@@ -0,0 +1,58 @@
+import { MonacoEditorLanguageClientWrapper, UserConfig } from 'monaco-editor-wrapper';
+import { configureWorker, defineUserServices } from './setupCommon.js';
+
+export const setupConfigExtended = (): UserConfig => {
+    const extensionFilesOrContents = new Map();
+    extensionFilesOrContents.set('/language-configuration.json', new URL('../language-configuration.json', import.meta.url));
+    extensionFilesOrContents.set('/yadl-grammar.json', new URL('../syntaxes/yadl.tmLanguage.json', import.meta.url));
+
+    return {
+        wrapperConfig: {
+            serviceConfig: defineUserServices(),
+            editorAppConfig: {
+                $type: 'extended',
+                languageId: 'yadl',
+                code: `// YADL is running in the web!`,
+                useDiffEditor: false,
+                extensions: [{
+                    config: {
+                        name: 'yadl-web',
+                        publisher: 'generator-langium',
+                        version: '1.0.0',
+                        engines: {
+                            vscode: '*'
+                        },
+                        contributes: {
+                            languages: [{
+                                id: 'yadl',
+                                extensions: [
+                                    '.yadl'
+                                ],
+                                configuration: './language-configuration.json'
+                            }],
+                            grammars: [{
+                                language: 'yadl',
+                                scopeName: 'source.yadl',
+                                path: './yadl-grammar.json'
+                            }]
+                        }
+                    },
+                    filesOrContents: extensionFilesOrContents,
+                }],                
+                userConfiguration: {
+                    json: JSON.stringify({
+                        'workbench.colorTheme': 'Default Dark Modern',
+                        'editor.semanticHighlighting.enabled': true
+                    })
+                }
+            }
+        },
+        languageClientConfig: configureWorker()
+    };
+};
+
+export const executeExtended = async (htmlElement: HTMLElement) => {
+    const userConfig = setupConfigExtended();
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    await wrapper.initAndStart(userConfig, htmlElement);
+};
