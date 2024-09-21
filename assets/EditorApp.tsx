fcbe6a57686a16371213d89ee124d0d13df14b2b
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2024-09-21 18:30:23 +0530

    Added icon parsing

--- assets/EditorApp.tsx
+++ assets/EditorApp.tsx
@@ -5,7 +5,7 @@ import {
   deserializeAST,
   DocumentChangeResponse,
 } from "langium-ast-helper";
-import { getMainTreeNode, YadlModelAstNode } from "utils/YADLDeserializer";
+import { getYADLNodes, YadlModelAstNode } from "utils/YADLDeserializer";
 
 export default function EditorApp() {
   // const monacoEditor: React.RefObject<MonacoEditorReactComp>;
@@ -14,12 +14,12 @@ export default function EditorApp() {
       <h3>Editor App</h3>
       <Editor
         onChange={(resp: DocumentChangeResponse) => {
-          // console.error(resp.content)
+          console.error(JSON.parse(resp.content))
           const ast = deserializeAST(resp.content) as YadlModelAstNode;
-          const deserializedContent = getMainTreeNode(ast);
+          const deserializedContent = getYADLNodes(ast);
           console.log(` $$$$ AST = ${JSON.stringify(deserializedContent, null, 2)}`);
           // console.log(` $$$$ AST = ${JSON.stringify(ast.enums)}`);
-          // console.log(` $$$$ AST = ${JSON.stringify(getMainTreeNode(resp), null, 2)}`);
+          // console.log(` $$$$ AST = ${JSON.stringify(getYADLNodes(resp), null, 2)}`);
         }}
       />
     </div>
--- assets/utils/YADLDeserializer.ts
+++ assets/utils/YADLDeserializer.ts
@@ -1,40 +1,29 @@
 import { AstNode } from "langium-ast-helper";
-// import { TreeNode } from "../d3tree";
 
-export function getMainTreeNode(ast: AstNode): YadlModelAstNode {
-//   console.error(JSON.stringify(ast, null, 2));
-
+export function getYADLNodes(ast: AstNode): YadlModelAstNode {
+  //   console.error(JSON.stringify(ast, null, 2));
   const astNode = getYadlModelAst(ast as YadlModelAstNode);
+  //   console.error(JSON.stringify(astNode.enums.length, null, 2));
 
-//   console.error(JSON.stringify(astNode.enums.length, null, 2));
-
-
-    const getEnumTypeTreeNode = (d: Enums) : Enums => {
-      return {
-        name: d.name, 
-        $type: "Enum",
-        // children: [],
-      };
-    };
-
-    const enums = astNode.enums.flatMap((e) => getEnumTypeTreeNode(e));
-
-    // console.error(JSON.stringify(enums, null, 2));
-    // console.error(` $$$$ Enums: ${JSON.stringify(enums, null, 2)}`);
-  //   const children: TreeNode[] = [];
-
-  //   if (enums.length > 0) {
-  //     children.push({ name: "Enum", $type: "Enum", children: enums });
-  //   }
-
+  const enums = astNode.enums.flatMap((d: Enums) => {
     return {
-      name: astNode.$type,
-      $type: astNode.$type,
-      enums: enums,
-    //   children: [],
+      name: d.name,
+      $type: "Enum",
     };
-//   return astNode;
+  });
+  const icons = astNode.icons.flatMap((i: Icon) => {
+    return {
+      icon: i.icon,
+      $type: "Icon",
+    };
+  });
 
+  return {
+    name: astNode.$type,
+    $type: astNode.$type,
+    enums: enums,
+    icons: icons,
+  };
 }
 
 /**
@@ -44,9 +33,10 @@ export function getYadlModelAst(ast: YadlModelAstNode): YadlModelAstNode {
   return {
     name: ast.name,
     $type: "YadlModel",
-    enums: (ast.enums as YadlModelElement[]).filter(
+    enums: (ast.enums as YadlModelElement[])?.filter(
       (e) => e.$type === "Enum",
     ) as Enums[],
+    icons: (ast.icons as Icon[])?.filter((e) => e.$type === "Icon") as Icon[],
   };
 }
 
@@ -54,19 +44,19 @@ export function getYadlModelAst(ast: YadlModelAstNode): YadlModelAstNode {
 export interface YadlModelAstNode extends AstNode, YadlModelElement {
   $type: "YadlModel";
   enums: Enums[];
+  icons: Icon[];
 }
 
 export interface Enums extends YadlModelElement {
-  $type: "Enum";
+  $type: string;
+}
+
+export interface Icon {
+  $type: string;
+  icon: string;
 }
 
 export interface YadlModelElement {
   $type: string;
   name: string;
 }
-
-// // create a union type of all possible YadlModelElement types
-// export type DomainModelElementType = Enums | YadlModelAstNode;
-
-// // create a union type of all possible YadlModelElement types names (string)
-// export type DomainModelElementTypeNames = DomainModelElementType["$type"];
