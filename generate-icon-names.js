149e285131f95fd08b72e7c6c9ba2e2113f77308
Sagar Shelar <sagarshelar@Sagars-MacBook-Pro.local>
2024-09-22 09:09:11 +0530

    minor change

--- generate-icon-names.js
+++ generate-icon-names.js
@@ -1,8 +1,23 @@
 const fs = require("fs");
-const readline = require("readline");
+const path = require("path");
 
-const input_path = "./assets/svgIcons/skill-icons/index.ts";
-const output_path = "./assets/utils/IconNames.ts";
+const getIndexFiles = function (dirPath, arrayOfFiles) {
+  files = fs.readdirSync(dirPath);
+  arrayOfFiles = arrayOfFiles || [];
+  files.forEach(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getIndexFiles(dirPath + "/" + file, arrayOfFiles);
+    } else {
+      if (file === "index.ts") {
+        const filePath = path.join(__dirname, dirPath, "/", file);
+        // console.log(filePath);
+        arrayOfFiles.push(filePath);
+      }
+    }
+  });
+
+  return arrayOfFiles;
+};
 
 const toKebabCase = (str) =>
   str &&
@@ -11,19 +26,51 @@ const toKebabCase = (str) =>
     .map((x) => x.toLowerCase())
     .join("-");
 
-const inputStream = fs.createReadStream(input_path);
-const outputStream = fs.createWriteStream(output_path, { encoding: "utf8" });
-var lineReader = readline.createInterface({
-  input: inputStream,
-  terminal: false,
+const indexFilePaths = getIndexFiles("./assets/svgIcons");
+// console.log(indexFilePaths);
+let allIcons = [];
+indexFilePaths.map((filePath) => {
+  var lines = fs.readFileSync(filePath, "utf-8").split("\n").filter(Boolean);
+  allIcons = allIcons.concat(lines);
 });
+// console.log(allIcons);
+
+const output_path = "./assets/utils/IconNames.ts";
+const outputStream = fs.createWriteStream(output_path, { encoding: "utf8" });
 outputStream.write("export const IconNames = {\n")
-lineReader.on("line", function (line) {
-  let componentName = line.substring(20);
+allIcons.map((iconPath) => {
+  let componentName = iconPath.substring(20);
   componentName = componentName.substring(0, componentName.indexOf("}") - 1);
-  const fullComponentName = `"${toKebabCase(componentName)}": "${componentName}",`
+  const fullComponentName = `"${toKebabCase(componentName)}": "${componentName}",`;
+  // console.log(fullComponentName);
   outputStream.write(fullComponentName + "\n");
 });
-lineReader.on("close", function () {
-  outputStream.write("};\n")
-})
+outputStream.write("};\n");
+
+
+// const input_path = "./assets/svgIcons/skill-icons/index.ts";
+// const output_path = "./assets/utils/IconNames.ts";
+
+// const toKebabCase = (str) =>
+//   str &&
+//   str
+//     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+//     .map((x) => x.toLowerCase())
+//     .join("-");
+
+// const inputStream = fs.createReadStream(input_path);
+// const outputStream = fs.createWriteStream(output_path, { encoding: "utf8" });
+// var lineReader = readline.createInterface({
+//   input: inputStream,
+//   terminal: false,
+// });
+// outputStream.write("export const IconNames = {\n")
+// lineReader.on("line", function (line) {
+//   let componentName = line.substring(20);
+//   componentName = componentName.substring(0, componentName.indexOf("}") - 1);
+//   const fullComponentName = `"${toKebabCase(componentName)}": "${componentName}",`
+//   outputStream.write(fullComponentName + "\n");
+// });
+// lineReader.on("close", function () {
+//   outputStream.write("};\n")
+// })
